b741b0fe1240e9d84af6a37ae729952c8cd49cef
Peter Pauly <p.pauly@tu-bs.de>
2021-05-31 18:41:45 +0200

    first functions

---
+++ src/crop-factor.js
@@ -0,0 +1,27 @@
+// provides methods concerning the crop factor
+
+exports.sensor_size = {	// from wikipedia
+	"35mm": (24, 36),
+	"APS-H": (19, 28.7),
+	"APS-C NPS": (15.7, 23.6),
+	"APS-C C": (14.8, 22.2),
+	"APS-C": (15, 22.5),
+	"4/3": (13.5, 18),
+	'1"': (8.8, 13.2)
+}
+exports.equiv_focal_length = function (f, cf) {
+// return Math.round(f * cf)
+	return f * cf
+}
+
+exports.normal_focal_length = function (cf) {
+	return 0
+}
+
+exports.hyperfocal = function (f, aperture, cropFactor) {
+	return f * f / aperture / coc
+}
+
+coc = function (diag) {
+	return diag / 1730
+}
---
+++ src/index.js
@@ -0,0 +1,3 @@
+CROP = require('./crop-factor')
+
+console.log(CROP.equiv_focal_length(10, 1.6))
