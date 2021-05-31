db3eb97fbf06442392ce805b670131d15d5098d1
Peter Pauly <p.pauly@tu-bs.de>
2021-05-31 19:32:40 +0200

    further function definitions

--- src/crop-factor.js
+++ src/crop-factor.js
@@ -9,6 +9,7 @@ sensor_sizes = {	// from wikipedia
 	"4/3": [13.5, 18],
 	'1"': [8.8, 13.2]
 }
+
 exports.equiv_focal_length = function (f, cf) {
 // return Math.round(f * cf)
 	return f * cf
@@ -18,10 +19,21 @@ exports.normal_focal_length = function (cf) {
 	return 0
 }
 
-exports.hyperfocal = function (f, aperture, cropFactor) {
-	return f * f / aperture / coc
+exports.hyperfocal = function (f, aperture, sensor) {
+	return f * f / aperture / coc(sensor)
 }
 
-coc = function (diag) {
+crop_factor = function (sensor) {
+	return this.diagonal(24, 36) / this.diagonal(sensor)
+}
+
+
+diagonal = function (sensor) {
+	sensor_size = sensor_sizes[sensor]
+	return Math.sqrt(sensor_size[0] ** 2 + sensor_size[1] ** 2)
+}
+
+coc = function (sensor) {
+	diag = diagonal(sensor)
 	return diag / 1730
 }
