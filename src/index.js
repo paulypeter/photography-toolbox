6a62d5402c9a660f2c82e406184c0f40b597f542
Peter Pauly <p.pauly@tu-bs.de>
2021-06-06 18:28:51 +0200

    adds lists of allowed ISO, aperture and shutter speed settings

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const CROP = require('./crop-factor')
 const FLASH = require('./flash')
 
 module.exports = {
+    // sensor
     sensor_sizes,
     coc,
     diagonal,
@@ -9,9 +10,15 @@ module.exports = {
     hyperfocal,
     equiv_focal_length,
     normal_focal_length,
+    // flash
     guide_number,
     flash_max_distance,
-    flash_aperture
+    flash_aperture,
+    // exposure
+    ISO_SPEEDS,
+    SHUTTER_SPEEDS,
+    APERTURES,
+    number_of_third_steps
 }
 
 console.log(crop_factor(sensor_sizes["APS-C C"]))
