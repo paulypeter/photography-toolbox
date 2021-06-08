79a67712da33abf130ce4393b4afbb8e54b8a469
Peter Pauly <p.pauly@tu-bs.de>
2021-06-08 13:51:53 +0200

    tests some more functions

--- test/index.test.js
+++ test/index.test.js
@@ -10,6 +10,7 @@ test("Returns correct sensor size", () => {
     expect(photographyTools.sensor_sizes['1"']).toStrictEqual([8.8, 13.2]);
 });
 
+// crop factor
 test("Returns correct crop factor", () => {
     expect(
         photographyTools.crop_factor(
@@ -62,4 +63,47 @@ test("Returns correct normal focal length", () => {
             photographyTools.sensor_sizes["APS-C C"]
         )
     ).toBe(27);
+});
+
+// exposure
+test("Returns correct stop distance", () => {
+    expect(
+        photographyTools.number_of_third_steps(
+            photographyTools.ISO_SPEEDS, 100, 400
+        )
+    ).toBe(6);
+    expect(
+        photographyTools.number_of_third_steps(
+            photographyTools.ISO_SPEEDS, 800, 250
+        )
+    ).toBe(-5);
+    expect(
+        photographyTools.number_of_third_steps(
+            photographyTools.SHUTTER_SPEEDS, "1/1000", "1/500"
+        )
+    ).toBe(3);
+    expect(
+        photographyTools.number_of_third_steps(
+            photographyTools.SHUTTER_SPEEDS, "1.3", "1/10"
+        )
+    ).toBe(-10);
+});
+
+// flash
+test("Returns correct maximum flash distance", () => {
+    expect(
+        photographyTools.flash_max_distance(
+            43, 10
+        )
+    ).toBe(4.3);
+    expect(
+        photographyTools.flash_max_distance(
+            47, 2
+        )
+    ).toBe(23.5);
+    expect(
+        photographyTools.flash_max_distance(
+            60, 4
+        )
+    ).toBe(15);
 });
