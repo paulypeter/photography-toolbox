fb388d97a1706f44e8054e1541e3d0128f2b9ff3
Peter Pauly <p.pauly@tu-bs.de>
2021-06-10 19:26:53 +0200

    rounds flash guide number

--- src/flash.js
+++ src/flash.js
@@ -1,7 +1,7 @@
 // defines methods for flash photography
 const EXPOSURE = require('./exposure')
 
-guide_number = (distance, f_stop) => distance * f_stop
+guide_number = (distance, f_stop) => Math.round(distance * f_stop)
 
 flash_max_distance = (guide_number, f_stop) => Number((guide_number / f_stop).toFixed(2))
 
