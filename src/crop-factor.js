64687b4207f9de34678ba46f6e05ae934715f270
Peter Pauly <p.pauly@tu-bs.de>
2021-06-12 17:07:20 +0200

    imports src files from main

---
+++ src/crop-factor.js
@@ -0,0 +1,65 @@
+// provides methods concerning the crop factor
+
+sensor_sizes = {	// from wikipedia
+	"35mm": [24, 36],
+	"APS-H": [19, 28.7],
+	"APS-C NPS": [15.7, 23.6],
+	"APS-C C": [14.8, 22.2],
+	"APS-C": [15, 22.5],
+	"4/3": [13.5, 18],
+	'1"': [8.8, 13.2]
+}
+
+// return Math.round(f * cf)
+equiv_focal_length = (focal_length, cf) =>  focal_length * cf
+
+normal_focal_length = sensor_size => Math.round(diagonal(sensor_size))
+
+hyperfocal = (focal_length, f_stop, sensor_size) => (focal_length ** 2) / f_stop / coc(sensor_size)
+
+crop_factor = sensor_size => Number((diagonal([24, 36]) / diagonal(sensor_size)).toFixed(1))
+
+diagonal = sensor_size => Math.sqrt(sensor_size[0] ** 2 + sensor_size[1] ** 2)
+
+coc = sensor_size => Number((diagonal(sensor_size) / 1500).toFixed(3))
+
+dof = (sensor_size, distance, f_stop, focal_length) => {
+	return (
+		Number(
+			(dof_far(sensor_size, distance, focal_length, f_stop) -
+				dof_near(sensor_size, distance, focal_length, f_stop)
+			).toFixed(2)
+		)
+	)
+}
+
+dof_near = (sensor_size, distance, focal_length, f_stop) => {
+	hyperfocal_dist = hyperfocal(focal_length, f_stop, sensor_size)
+	return Number(
+		(hyperfocal_dist * distance /
+			(hyperfocal_dist + distance - focal_length) / 1000
+		).toFixed(2)
+	)
+}
+
+dof_far = (sensor_size, distance, focal_length, f_stop) => {
+	hyperfocal_dist = hyperfocal(focal_length, f_stop, sensor_size)
+	return (
+		(hyperfocal_dist * distance /
+			(hyperfocal_dist - distance + focal_length) / 1000
+		).toFixed(2)
+	)
+
+}
+
+// angle of view for a given sensor dimension
+aov_dim = (dimension, focal_length) => Math.round(2 * Math.atan(dimension / (2 * focal_length)) * 180 / Math.PI)
+
+aov = (sensor_size, focal_length) => {
+	return [
+		aov_dim(sensor_size[0], focal_length),
+		aov_dim(sensor_size[1], focal_length),
+		aov_dim(diagonal(sensor_size), focal_length),
+	]
+}
+
---
+++ src/index.js
@@ -0,0 +1,34 @@
+const CROP = require('./crop-factor')
+const FLASH = require('./flash')
+const EXPOSURE = require('./exposure')
+
+module.exports = {
+    // sensor
+    sensor_sizes,
+    coc,
+    diagonal,
+    crop_factor,
+    hyperfocal,
+    equiv_focal_length,
+    normal_focal_length,
+    dof,
+    aov,
+    dof_near,
+    dof_far,
+    // flash
+    guide_number,
+    flash_max_distance,
+    flash_f_stop,
+    // exposure
+    ISO_SPEEDS,
+    SHUTTER_SPEEDS,
+    F_STOPS,
+    get_float_from_speed_str,
+    get_str_from_speed_float,
+    number_of_third_steps,
+    exposure_value,
+    find_nearest
+}
+
+console.log(crop_factor(sensor_sizes["APS-C C"]))
+console.log(exposure_value(8, 1 / 250, 400))
